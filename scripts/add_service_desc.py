fdc26b1e14a7bbf65a14e3c28359e00591bf00f8
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-30 16:02:29 -0400

    added saving to script

--- scripts/add_service_desc.py
+++ scripts/add_service_desc.py
@@ -4,20 +4,19 @@ from data_processor.models import ServiceDescription
 
 
 def run ():
+    name =price= description =google_play_link =ios_appstore_link =subscription_link =tooltip_title_link=slug = ""
     r = csv.DictReader(open('serv_desc.csv'))
     x = [i for i in r]
 
     for d in x:
         locals().update(d)
-
-        print(d)
-        # v = ServiceDescription()
-        # v.name = name
-        # v.price = price
-        # v.description = description
-        # v.google_play_link = google_play_link
-        # v.ios_appstore_link = ios_appstore_link
-        # v.subscription_link = subscription_link
-        # v.tooltip_tile_link = tooltip_tile_link
-        # v.slug = slug
-        # v.save()
+        v = ServiceDescription()
+        v.name = name
+        v.price = price
+        v.description = description
+        v.google_play_link = google_play_link
+        v.ios_appstore_link = ios_appstore_link
+        v.subscription_link = subscription_link
+        v.tooltip_tile_link = tooltip_title_link
+        v.slug = slug
+        v.save()
