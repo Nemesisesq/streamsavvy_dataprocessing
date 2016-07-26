66a07279847cfe40acd7aa1c9aed9cfe01ba3c8d
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-07-26 09:00:00 -0400

    removed stale dependecies in urls

--- streamsavvy_dataprocessing/urls.py
+++ streamsavvy_dataprocessing/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
 
 from data_processor.views import ServiceDescriptionViewSet, SearchContentViewSet, ChannelViewSet, ContentViewSet, \
     ViewingServicesViewSet, ModuleDescriptionViewSet
@@ -32,7 +31,6 @@ router.register(r'modules', ModuleDescriptionViewSet, 'moduledescriptions')
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'api-token-auth', obtain_jwt_token)
     # url(r'haystack_search', autocomplete, name='autocomplete'),
 
 ]
